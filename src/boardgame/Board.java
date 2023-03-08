913822b6c00d732e828436a93559d1c466942fe5
Gabriel Rameh <gabriel.rameh12@gmail.com>
2023-03-08 19:24:09 -0300

    ChessPosition

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -5,6 +5,9 @@ public class Board {
 	private int columns;
 	private Piece[][] pieces;
 	public Board(int rows, int columns) {
+		if(rows < 1 || columns < 1) {
+			throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
+		}
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
@@ -12,16 +15,13 @@ public class Board {
 	public int getRows() {
 		return rows;
 	}
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
 	public int getColumns() {
 		return columns;
 	}
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
 	public Piece piece(int row, int column) {
+		if(!positionExists(row, column)) {
+			throw new BoardException("Position not on the board");
+		}
 		return pieces[row][column];
 	}
 	public Piece piece(Position position) {
@@ -30,5 +30,14 @@ public class Board {
 	public void placePiece(Piece piece, Position position) {
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
+	}
+	private boolean positionExists(int row, int column) {
+		return row >= 0 && row < rows && column >= 0 && column < columns;
+	}
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumn());
+	}
+	public boolean thereIsAPiece(Position position) {
+		return piece(position) != null;
 	}
 }
---
+++ src/chess/ChessPosition.java
@@ -0,0 +1,30 @@
+package chess;
+
+import boardgame.Position;
+
+public class ChessPosition {
+	private char column;
+	private int row;
+	public ChessPosition(char column, int row) {
+		super();
+		this.column = column;
+		this.row = row;
+	}
+	public char getColumn() {
+		return column;
+	}
+	public int getRow() {
+		return row;
+	}
+	protected Position toPosition() {
+		return new Position(8 - row, column - 'a');
+	}
+	protected static ChessPosition fromPosition(Position position) {
+		return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
+	}
+	@Override
+	public String toString() {
+		return "" + column + row;
+	}
+	
+}
